0754c0326a67b1cfad168fb28ee7175b9eb30d62
Michael Rolle <michael.rolle@bbd.co.za>
2024-04-29 21:29:03 +0200

    Setup terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "common_tags" {
+  type        = map(string)
+  description = "Common tags applied to all resources"
+}
+
+variable "region" {
+  type        = string
+  description = "The region where the resources will be deployed."
+}
+
+variable "cors_allowed_origins" {
+  type = list(string)
+  description = "The domains allowed in the CORS configuration."
+}
+
+variable "bucket_name" {
+  type = string
+  description = "Name of the bucket."
+}
