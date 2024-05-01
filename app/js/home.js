ed2c2bbd87d19bd487cf2b7760c81c79280573b7
Michael Rolle <157356598+Michael-Rolle-BBD@users.noreply.github.com>
2024-05-01 18:30:49 +0200

    Feature/michael/web cards (#3)
    * Initial card design done
    
    * Added src of profile pics to the backend photo bucket
    
    * Finished navbar
    
    * Moved login css to its own file

--- app/js/home.js
+++ app/js/home.js
@@ -1,14 +1,3 @@
 
 export function home() {
-  const lol = document.getElementById('lol');
-
-  setInterval(async () =>{    
-    const response = await fetch('https://api.chucknorris.io/jokes/random');
-    const text = await response.json();
-    
-    const p = document.createElement('p');
-    p.innerText = text.value;
-
-    lol.append(p);
-  }, 2500);
 }
