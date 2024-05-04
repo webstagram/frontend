fa9a11720b20f2bcfc7b88adf717bb83ce616b61
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-05-04 10:21:19 +0200

    Revert "Lesedi/fix/investigate navigation error when refreshing"

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,10 @@
 import { login } from "./login.js";
 import { home } from "./home.js";
-import { closeLoadingScreen } from "./util/loading.js";
 
 const route = (event) => {
   event = event || window.event;
   event.preventDefault();
   window.history.pushState({}, '', event.target.href);
-  console.log(event.target.href);
   handleLocation();
 };
 
@@ -34,15 +32,23 @@ const routes = {
 };
 
 const handleLocation = async () => {
-  const path = window.location.hash.slice(1);
+  const path = window.location.pathname;
   const route = (!!routes[path] && routes[path].template) || routes[404].template;
   const html = await fetch(route).then((data) => data.text());
   document.getElementById('main-page').innerHTML = html;
-  !!route[path] && routes[path].js(path);
+  routes[path].js(path);
 };
+
+const addClickEventToNavItems = () => {
+  const mainNav = document.getElementById('main-nav');
+  const children = mainNav.children;
+  for (let i = 0; i < children.length; i++) {
+    children[i].addEventListener('click', route);
+  }
+}
 
 window.onpopstate = handleLocation;
 window.route = route;
 
 handleLocation();
-closeLoadingScreen();
+addClickEventToNavItems();
--- app/js/login.js
+++ app/js/login.js
@@ -1,18 +1,12 @@
-import { goTo } from "./util/navigation.js";
-import { openLoadingScreen, closeLoadingScreen } from "./util/loading.js";
+
 
 export function login() {
   const loginBtn = document.getElementById("login-btn");
 
-  
   loginBtn.addEventListener("click", (event) => {
-    openLoadingScreen();
-  
-    setTimeout(() => {
-      closeLoadingScreen();
-
-      goTo('/home');
-    }, 4000);
-    
+    const a = document.createElement('a');
+    a.href = '/home';
+    a.onclick = window.route;
+    a.click();
   });
 }
