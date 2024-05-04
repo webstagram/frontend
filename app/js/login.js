77769f0aa6f9a7c90e6025c37a18da3f10e40c51
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-05-04 15:24:11 +0200

    Update login.js

--- app/js/login.js
+++ app/js/login.js
@@ -4,10 +4,7 @@ export function login() {
   const loginBtn = document.getElementById("login-btn");
 
   loginBtn.addEventListener("click", (event) => {
-    // GitHub's OAuth authorization endpoint
     const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${config.GITHUB_CLIENT_ID}`;
-  
-    // Redirect to GitHub's OAuth login page
     window.location.href = githubAuthUrl;
   });
 }
