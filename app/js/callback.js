89f81ba5746aefca78e6f7b0b0a9dbde9ec9f251
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-05-04 15:23:38 +0200

    cleanup

--- app/js/callback.js
+++ app/js/callback.js
@@ -1,19 +1,13 @@
 const queryString = window.location.search;
-import config from './js/configManager.js';
+import config from './configManager.js';
 const backendURL=config.BACKEND_URL;
-import { fetchWithAuth} from "./js/authRequest.js";
-
-// Use URLSearchParams to parse the query string
 const urlParams = new URLSearchParams(queryString);
+const paramValue = urlParams.get('code'); 
 
-// Get the parameter value
-const paramValue = urlParams.get('code'); // Replace 'param' with your parameter name
-
-// Print the parameter value in the body of the HTML page
 async function fetchResponse() {
     try {
       let response = await fetch(`${backendURL}github/callback?code=${paramValue}`);
-     response = await response.json(); // Make sure to await the .json() call as well
+     response = await response.json();
       console.log(response);
        localStorage.setItem('jwtToken', response.jwt);
     } catch (error) {
@@ -21,7 +15,6 @@ async function fetchResponse() {
     }
   }
   
-  // Call the async function
 (async () => {
   try {
     await fetchResponse();
