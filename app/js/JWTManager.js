5eedbc008d4023a6687d1810fed1f2e59489a3a0
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-05-05 21:20:38 +0200

    Update JWTManager.js

--- app/js/JWTManager.js
+++ app/js/JWTManager.js
@@ -1,11 +1,28 @@
 const token = localStorage.getItem('jwtToken');
 function decodeJWT() {
-    return JSON.parse(atob(token.split('.')[1]));
+    try {
+    const data=token.split('.')[1];
+    const stringify=atob(base64UrlToBase64(data));
+    return JSON.parse(stringify);
+    } catch (error) {
+        return null;
+    }
+    
 }
 function isTokenExpired() {
     if (!token) return true;
     const payload = decodeJWT();
-    return Date.now() >= (payload.exp * 1000) - 30000;
+    return  !payload || Date.now() >= (payload.exp * 1000) - 30000;
 }
-
+function base64UrlToBase64(input) {
+    let base64String = input.replace(/-/g, '+').replace(/_/g, '/');
+    let padding = 4 - (base64String.length % 4);
+    if (padding !== 4) {
+      for (let i = 0; i < padding; i++) {
+        base64String += '=';
+      }
+    }
+    return base64String;
+  }
+  
 export { isTokenExpired, decodeJWT }
