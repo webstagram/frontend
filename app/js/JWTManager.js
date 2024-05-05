3ca552633335924474268c324002b037422a9fd1
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-05-05 21:29:58 +0200

    Update JWTManager.js

--- app/js/JWTManager.js
+++ app/js/JWTManager.js
@@ -1,28 +1,34 @@
 const token = localStorage.getItem('jwtToken');
 function decodeJWT() {
     try {
-    const data=token.split('.')[1];
-    const stringify=atob(base64UrlToBase64(data));
-    return JSON.parse(stringify);
+        const data = token.split('.')[1];
+        const stringify = (base64UrlDecode(data));
+        return JSON.parse(stringify);
     } catch (error) {
+        console.log(error)
         return null;
     }
-    
+
 }
 function isTokenExpired() {
     if (!token) return true;
     const payload = decodeJWT();
-    return  !payload || Date.now() >= (payload.exp * 1000) - 30000;
+    return !payload || Date.now() >= (payload.exp * 1000) - 30000;
 }
-function base64UrlToBase64(input) {
+function base64UrlDecode(input) { //required as atob only works with ASCII characters
     let base64String = input.replace(/-/g, '+').replace(/_/g, '/');
     let padding = 4 - (base64String.length % 4);
     if (padding !== 4) {
-      for (let i = 0; i < padding; i++) {
-        base64String += '=';
-      }
+        for (let i = 0; i < padding; i++) {
+            base64String += '=';
+        }
     }
-    return base64String;
-  }
-  
+    const decodedData = atob(base64String);
+    const decodedString = decodeURIComponent(Array.from(decodedData).map((c) => {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return decodedString;
+}
+
+
 export { isTokenExpired, decodeJWT }
