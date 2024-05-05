e2a08a862524ddeebab677b2ed6b6a7489fdb89f
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-05-05 10:11:22 +0200

    in progress

--- app/js/app.js
+++ app/js/app.js
@@ -46,6 +46,12 @@ const handleLocation = async () => {
   const urlParams = new URLSearchParams(window.location.search);
   let path = urlParams.get('path');
   if(!path)path='';
+  if(!isTokenExpired() && path===''){
+    path='home';
+  }
+  else if( isTokenExpired()&& !(path==='about')){
+    path='';
+  }
   const route = (!!routes[path] && routes[path].template) || routes[404].template;
   const html = await fetch(route).then((data) => data.text());
   document.getElementById('main-page').innerHTML = html;
@@ -66,11 +72,21 @@ window.route = route;
 if(!isTokenExpired()){
   document.getElementById("main-profile-image").src=decodeJWT().userImage;
   document.getElementById("username").textContent=decodeJWT().userName;
+  document.getElementById("main-profile-image").style.display = 'grid';
+  document.getElementById("username").style.display = 'grid';
 
   }
   else{
     document.getElementById("main-profile-image").style.display = 'none';
     document.getElementById("username").style.display = 'none';
   }
+  const webLogo = document.getElementById("webstagram-logo");
+
+  webLogo.addEventListener("click", (event) => {
+    const a = document.createElement('a');
+    a.href = '/?path=about';
+    a.onclick = window.route;
+    a.click();
+  });  
 handleLocation();
 addClickEventToNavItems();
